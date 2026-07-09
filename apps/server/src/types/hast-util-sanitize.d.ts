30a67cc86a1f738df4344629c8673f3b4315fc2f
cropflre <767308865@qq.com>
2026-07-09 22:57:46 +0800

    fix: add missing prerender type declarations

---
+++ apps/server/src/types/hast-util-sanitize.d.ts
@@ -0,0 +1,4 @@
+declare module 'hast-util-sanitize' {
+  export const defaultSchema: any;
+  export type Schema = any;
+}
---
+++ apps/server/src/types/rehype-stringify.d.ts
@@ -0,0 +1,5 @@
+declare module 'rehype-stringify' {
+  import { Plugin } from 'unified';
+  const rehypeStringify: Plugin;
+  export default rehypeStringify;
+}

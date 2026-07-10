71718f8fc83c424d283cbaea43ec188bea5ec153
Drug <cropflre2333@gmail.com>
2026-07-10 11:43:20 +0800

    feat: start scheduled publishing worker (BLOG-15.2)

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,8 +2,10 @@ import { serve } from '@hono/node-server';
 import { app } from './app';
 import { initDb } from './db/init';
 import { env } from './config/env';
+import { startScheduledPublisher } from './modules/admin-posts/scheduled-publisher';
 
 initDb();
+startScheduledPublisher();
 
 serve({ fetch: app.fetch, port: env.port }, (info) => {
   console.log(`🚀 Blog API listening on http://localhost:${info.port}`);
